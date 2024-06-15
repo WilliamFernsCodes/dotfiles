66b100e33382d1249bc322b2b988ff297d93aa99
WilliamFernsCodes <williamfernscodes@gmail.com>
2024-06-15 16:29:12 +0200

    fix(scripts): changed show keybindings to show all text

--- scripts/show_keybindings.sh
+++ scripts/show_keybindings.sh
@@ -3,8 +3,8 @@
 # Path to your i3 config file
 CONFIG_FILE=~/.config/i3/config
 
-# Extract keybindings from the config file
-grep "^bindsym" $CONFIG_FILE | sed 's/^bindsym //' | awk '{print $1, $2, $3}' > /tmp/keybindings.txt
+# Extract keybindings from the config file and remove the 'bindsym' keyword
+grep "^bindsym" $CONFIG_FILE | sed 's/^bindsym //' > /tmp/keybindings.txt
 
 # Display keybindings using rofi
 rofi -dmenu -i -l 20 -p "i3 Keybindings" < /tmp/keybindings.txt
