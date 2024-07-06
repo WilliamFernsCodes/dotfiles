50e291162a06c43e6d78b8fb59c4859e04442560
WilliamFernsCodes <williamfernscodes@gmail.com>
2024-07-06 16:22:02 +0200

    feat(mappings): fixed mappings, also to show on cheatcheet

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -7,11 +7,6 @@ local function refactor_with_args(command)
   vim.cmd(command .. " " .. args)
 end
 
-M.general = {
-  n = {
-  }
-}
-
 M.navigation = {
   n = {
     ['C-h'] = { "<cmd> TmuxNavigateLeft<CR>", "window left" },
@@ -101,19 +96,111 @@ M.harpoon = {
   }
 }
 
+
+local function toggle_breakpoint()
+  local width = 30
+  local height = 1
+  local buf = vim.api.nvim_create_buf(false, true)
+  local win = require("plenary.popup").create(buf, {
+    title = 'Enter breakpoint condition',
+    highlight = 'Normal',
+    line = math.floor((vim.o.lines - height) / 2),
+    col = math.floor((vim.o.columns - width) / 2),
+    minwidth = width,
+    minheight = height,
+    border = {},
+  })
+
+  local function close_popup()
+    vim.api.nvim_win_close(win, true)
+    vim.api.nvim_buf_delete(buf, { force = true })
+  end
+
+  vim.api.nvim_buf_set_option(buf, 'modifiable', true)
+  vim.api.nvim_buf_set_option(buf, 'buftype', 'prompt')
+  vim.fn.prompt_setprompt(buf, 'Condition: ')
+
+  vim.api.nvim_buf_set_keymap(buf, 'i', '<CR>', '', {
+    noremap = true,
+    callback = function()
+      local input = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+      close_popup()
+      require("dap").toggle_breakpoint("'" .. input .. "'")
+    end,
+  })
+
+  vim.api.nvim_buf_set_keymap(buf, 'n', '<ESC>', '', {
+    noremap = true,
+    callback = close_popup,
+  })
+
+  vim.cmd('startinsert')
+end
+
+
 M.dap = {
   plugin = true,
   n = {
-    ["<leader>db"] = { "<cmd> DapToggleBreakpoint <CR>" },
-    ["<leader>dc"] = { "<cmd> DapContinue <CR>" },
-    ["<leader>dh"] = { "<cmd> DapToggleHover <CR>" },
-    ["<leader>di"] = { "<cmd> DapStepInto <CR>" },
-    ["<leader>do"] = { "<cmd> DapStepOut <CR>" },
-    ["<leader>dr"] = { "<cmd> DapRestart <CR>" },
-    ["<leader>ds"] = { "<cmd> DapStart <CR>" },
-    ["<leader>dt"] = { "<cmd> DapToggle <CR>" },
-    ["<leader>du"] = { "<cmd> DapRunToCursor <CR>" },
-    ["<leader>dx"] = { "<cmd> DapTerminate <CR>" },
+    ["<leader>cbd"] = {
+      function()
+        toggle_breakpoint()
+      end,
+      "Toggle conditional breakpoint"
+    },
+    ["<leader>db"] = {
+      function()
+        vim.cmd("DapToggleBreakpoint")
+      end,
+      "DapToggleBreakpoint"
+    },
+    ["<leader>dcc"] = {
+      function()
+        vim.cmd("DapContinue")
+      end,
+      "DapContinue"
+    },
+    ["<leader>dh"] = {
+      function()
+        vim.cmd("DapToggleHover")
+      end,
+      "DapToggleHover"
+    },
+    ["<leader>di"] = {
+      function()
+        vim.cmd("DapStepInto")
+      end,
+      "DapStepInto"
+    },
+    ["<leader>do"] = {
+      function()
+        vim.cmd("DapStepOut")
+      end,
+      "DapStepOut"
+    },
+    ["<leader>dr"] = {
+      function()
+        vim.cmd("DapRestart")
+      end,
+      "DapRestart"
+    },
+    ["<leader>ds"] = {
+      function()
+        vim.cmd("DapStart")
+      end,
+      "DapStart"
+    },
+    ["<leader>dt"] = {
+      function()
+        vim.cmd("DapToggle")
+      end,
+      "DapToggle"
+    },
+    ["<leader>du"] = {
+      function()
+        vim.cmd("DapRunToCursor")
+      end,
+      "DapRunToCursor"
+    }
   }
 }
 
@@ -123,7 +210,8 @@ M.dap_python = {
     ["<leader>dpr"] = {
       function()
         require("dap-python").test_method()
-      end
+      end,
+      "DapPythonRun"
     },
   }
 }
@@ -133,14 +221,16 @@ M.telescope = {
     ["<leader>rr"] = {
       function()
         require('telescope').extensions.refactoring.refactors()
-      end
+      end,
+      "TelescopeRefactoringTool"
     }
   },
   s = {
     ["<leader>rr"] = {
       function()
         require('telescope').extensions.refactoring.refactors()
-      end
+      end,
+      "TelescopeRefactoringTool"
     }
   }
 }
